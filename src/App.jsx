3b1f63d43c4b195b588e99dccd2604f7af60e7ee
Ansh-3094 <anshprajapati074@gmail.com>
2026-02-27 12:20:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="bg-amber-600">heyyy</h1>
+      <p>Ansh prajapati</p>
+    </>
+  );
+}
+
+export default App;
